8327b1dae53ea3f0d6703838a1172a672e3d63ed
Javier Pizarro <jpizarro@iMac-de-Javier.local>
2021-03-25 21:08:05 +0100

    little drawing program

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jpizarro <jpizarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 20:07:11 by jpizarro          #+#    #+#             */
-/*   Updated: 2021/03/25 13:13:31 by jpizarro         ###   ########.fr       */
+/*   Updated: 2021/03/25 21:03:27 by jpizarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,42 +32,56 @@
 //	return (0);
 //}
 
-// Sending motion images to the window
+//	Sending motion images to the window and managing with the keyboard.
+//	choose a color (1 to 8 keys), and click over the window with the left or right button.
+//	Drag the shape where you want, change its color, move it with the wasd keys.
+//	Press 'e' to keep it or 'q' to erase it.
+//	Adjust it with the WASD keys, and fix it wiht the E.
 
 typedef struct s_img
 {
 	void		*ptr;
 	char		*addr;
-	int			bpp;
-	int			lnlen;
-	int			endian;
+	int	bpp;
+	int	lnlen;
+	int	endian;
 }			t_img;
+
+typedef struct s_shape
+{
+	char		type;
+	int			x;
+	int			y;
+	int			mov_x;
+	int			mov_y;
+	int			size;
+}			t_shape;
 
 typedef struct s_vars
 {
 	void		*mlx_ptr;
 	void		*win_ptr;
+	t_img		img0;
 	t_img		img1;
-	t_img		img2;
-	int			img;
+	t_shape		shape;
+	char		img;
 	int			x_dim;
 	int			y_dim;
 	unsigned	color;
 }			t_vars;
 
-typedef struct s_square
-{
-	int			x0;
-	int			x1;
-	int			y0;
-	int			y1;
-}			t_square;
-
 int	press_key(int key_code, t_vars *vars);
-//int	release_key(int key_code, t_vars *vars);
+void	change_color(int key_color, t_vars *vars);
+int	wasdeq(int key_code, t_vars *vars);
+int	release_key(int key_code, t_vars *vars);
 int	window_area(int key_code, t_vars *vars);
-int	square_paint(t_square sq, t_vars *vars);
+int	square_paint(t_vars *vars);
+int	circle_paint(t_vars *vars);
+void	fix_shape(t_vars *vars);
+void	erase_shape(t_vars *vars);
 void	pixel_push(t_vars *vars, int x, int y);
+void	copy_img(t_vars *vars);
+
 
 
 int	press_key(int key_code, t_vars *vars)
@@ -77,9 +91,20 @@ int	press_key(int key_code, t_vars *vars)
 //	printf("color: %u\n", vars->color);
 //	printf("win_ptr = %p\n", vars->win_ptr);
 
-	if (key_code == 53)
+	if ((key_code >= 12 && key_code <= 14) || (key_code >= 0 && key_code <= 2))
+		wasdeq(key_code, vars);
+	else if (key_code == 53)
 		mlx_destroy_window(vars->mlx_ptr, vars->win_ptr);
-	else if (key_code == 28)
+	else if ((key_code >= 18 && key_code <= 23) || key_code == 26 || key_code == 28)
+		change_color(key_code, vars);
+	else if ((key_code <= 126 && key_code >= 123) || key_code == 49)
+		window_area(key_code, vars);
+	return (0);
+}
+
+void	change_color(int key_code, t_vars *vars)
+{
+	if (key_code == 28)
 		vars->color = 0x00ffffff;
 	else if (key_code == 18)
 		vars->color = 0x00ff0000;
@@ -95,76 +120,128 @@ int	press_key(int key_code, t_vars *vars)
 		vars->color = 0x00ffff00;
 	else if (key_code == 26)
 		vars->color = 0x00000000;
-	else if ((key_code <= 126 && key_code >= 123) || key_code == 49)
-		window_area(key_code, vars);
+	if (vars->img == 1 || vars->img == 2)
+	{
+		copy_img(vars);
+		if (vars->shape.type == 's')
+			square_paint(vars);
+		else if (vars->shape.type == 'c')
+			circle_paint(vars);
+	}
+}
+
+int	wasdeq(int key_code, t_vars *vars)
+{
+
+	if (key_code == 12)
+	{
+		erase_shape(vars);
+		return (0);
+	}
+	if (vars->img == 2)
+	{
+		if (key_code == 14)
+		{
+			fix_shape(vars);
+			return (0);
+		}
+		else if (key_code == 13)
+			vars->shape.mov_y = -1;
+		else if (key_code == 0)
+			vars->shape.mov_x = -1;
+		else if (key_code == 1)
+			vars->shape.mov_y = 1;
+		else if (key_code == 2)
+			vars->shape.mov_x = 1;
+	}
 	return (0);
 }
 
-//int	release_key(int key_code, t_vars *vars)
-//{
+int	release_key(int key_code, t_vars *vars)
+{
 //	printf("Hello from release_key\n");
 //	printf("key_code = %i\n", key_code);
-//	printf("Color changed to %x\n", vars->color);
-//	return (0);
-//}
-
-//int	window_area(int key_code, t_vars vars)
-//{
-//	t_square area;
-//
-//	area.x0 = 0;
-//	area.x1 = vars.x_dim;
-//	area.y0 = 0;
-//	area.y1 = vars.y_dim;
-//	if (key_code == 126)
-//		area.y1 = vars.y_dim / 2;
-//	else if (key_code == 125)
-//		area.y0 = vars.y_dim / 2;
-//	else if (key_code == 123)
-//		area.x1 = vars.x_dim/ 2;
-//	else if (key_code == 124)
-//		area.x0 = vars.x_dim / 2;
-//	square_paint(area, vars);
-//	return (0);
-//}
+	if (key_code == 13)
+		vars->shape.mov_y = 0;
+	else if (key_code == 0)
+		vars->shape.mov_x = 0;
+	else if (key_code == 1)
+		vars->shape.mov_y = 0;
+	else if (key_code == 2)
+		vars->shape.mov_x = 0;
+	return (0);
+}
 
 int	window_area(int key_code, t_vars *vars)
 {
-//	printf("Hello from window_area\n");
+	short int	x0;
+	short int	x1;
+	short int	y0;
+	short int	y1;
+	short int	i;
+	short int	j;
 
-	t_square area;
+	vars->img = 0;
+	x0 = 0 +  (vars->x_dim / 2) * (key_code == 124);
+	x1 = vars->x_dim / (1 + (key_code == 123));
+	y0 = 0 +  (vars->y_dim / 2) * (key_code == 125);
+	y1 = vars->y_dim / (1 + (key_code == 126));
 
-	area.x0 = 0 +  (vars->x_dim / 2) * (key_code == 124);
-	area.x1 = vars->x_dim / (1 + (key_code == 123));
-	area.y0 = 0 +  (vars->y_dim / 2) * (key_code == 125);
-	area.y1 = vars->y_dim / (1 + (key_code == 126));
-
-//	printf("x0 = %i, x1 = %i, y0 = %i, y1 = %i\n", area.x0, area.x1, area.y0, area.y1);
-
-
-	square_paint(area, vars);
-	return (0);
-}
-
-int square_paint(t_square sq, t_vars *vars)
-{
-//	printf("Hello form square_paint\n");
-
-	int i;
-	int j;
-
-	j = sq.y0;
-	while (j < sq.y1)
+	j = y0;
+	while (j < y1)
 	{
-		i = sq.x0;
-		while (i < sq.x1)
+		i = x0;
+		while (i < x1)
 		{
 			pixel_push(vars, i, j);
 			i++;
 		}
 		j++;
 	}
-//	mlx_put_image_to_window(vars->mlx_ptr, vars->win_ptr, vars->img.ptr, 0, 0);
+	copy_img(vars);
+	return (0);
+}
+
+int square_paint(t_vars *vars)
+{
+	int i;
+	int j;
+
+	vars->shape.x += vars->shape.mov_x;
+	vars->shape.y += vars->shape.mov_y;
+	j = (vars->shape.y - vars->shape.size / 2) * (vars->shape.y >= vars->shape.size / 2 );
+	while (j < (vars->shape.y + vars->shape.size / 2) && j < vars->y_dim)
+	{
+		i = (vars->shape.x - vars->shape.size / 2) * (vars->shape.x >= vars->shape.size / 2 );
+		while (i < (vars->shape.x + vars->shape.size / 2) && i < vars->x_dim)
+		{
+			pixel_push(vars, i, j);
+			i++;
+		}
+		j++;
+	}
+	return (0);
+}
+
+int circle_paint(t_vars *vars)
+{
+	int i;
+	int j;
+
+	vars->shape.x += vars->shape.mov_x;
+	vars->shape.y += vars->shape.mov_y;
+	j = (vars->shape.y - vars->shape.size / 2) * (vars->shape.y >= vars->shape.size / 2 );
+	while (j < (vars->shape.y + vars->shape.size / 2) && j < vars->y_dim)
+	{
+		i = (vars->shape.x - vars->shape.size / 2) * (vars->shape.x >= vars->shape.size / 2 );
+		while (i < (vars->shape.x + vars->shape.size / 2) && i < vars->x_dim)
+		{
+			if ((i - vars->shape.x) * (i - vars->shape.x) + (j - vars->shape.y) * (j - vars->shape.y) <= (vars->shape.size / 2) * (vars->shape.size / 2))
+				pixel_push(vars, i, j);
+			i++;
+		}
+		j++;
+	}
 	return (0);
 }
 
@@ -172,23 +249,23 @@ void	pixel_push(t_vars *vars, int x, int y)
 {
 	char	*dst;
 
-	if (vars->img == 1)
+	if (vars->img == 0)
 	{
-		dst = vars->img1.addr + (y * vars->img1.lnlen + x * (vars->img1.bpp / 8));
+		dst = vars->img0.addr + (y * vars->img0.lnlen + x * (vars->img0.bpp / 8));
 		*(unsigned int*)dst = vars->color;
 	}
-	else if (vars->img == 2)
+	else if (vars->img == 1 || vars->img == 2)
 	{
-		dst = vars->img2.addr + (y * vars->img2.lnlen + x * (vars->img2.bpp / 8));
+		dst = vars->img1.addr + (y * vars->img1.lnlen + x * (vars->img1.bpp / 8));
 		*(unsigned int*)dst = vars->color;
 	}
 }
 
 void	copy_img(t_vars *vars)
 {
-	char	*dst;
-	int		x;
-	int		y;
+	char		*dst;
+	int	x;
+	int	y;
 
 	y = 0;
 	while (y < vars->y_dim)
@@ -196,8 +273,8 @@ void	copy_img(t_vars *vars)
 		x = 0;
 		while (x < vars->x_dim)
 		{
-			dst = vars->img2.addr + (y * vars->img2.lnlen + x * (vars->img2.bpp / 8));
-			*(unsigned int*)dst = *(unsigned int*)(vars->img1.addr + (y * vars->img1.lnlen + x * (vars->img1.bpp / 8)));
+			dst = vars->img1.addr + (y * vars->img1.lnlen + x * (vars->img1.bpp / 8));
+			*(unsigned int*)dst = *(unsigned int*)(vars->img0.addr + (y * vars->img0.lnlen + x * (vars->img0.bpp / 8)));
 			x++;
 		}
 		y++;
@@ -206,75 +283,96 @@ void	copy_img(t_vars *vars)
 
 int	no_event(t_vars *vars)
 {
+	
 //	static int i = 0;
 //	printf("Hello form no_event step %i\n", i++);
-	if (vars->img == 1)
-		mlx_put_image_to_window(vars->mlx_ptr, vars->win_ptr, vars->img1.ptr, 0, 0);
-	else if (vars->img == 2)
-		mlx_put_image_to_window(vars->mlx_ptr, vars->win_ptr, vars->img2.ptr, 0, 0);
+//	printf("Showing image %i\n", vars->img);
+//	if (vars->img == 0)
+//		mlx_put_image_to_window(vars->mlx_ptr, vars->win_ptr, vars->img0.ptr, 0, 0);
+//	else if (vars->img == 1 || vars->img == 2)
+	if (vars->img > 0 && (vars->shape.mov_x != 0 || vars->shape.mov_y != 0))
+	{
+		copy_img(vars);
+		if (vars->shape.type == 's')
+			square_paint(vars);
+		else if (vars->shape.type == 'c')
+			circle_paint(vars);
+	}
+	mlx_put_image_to_window(vars->mlx_ptr, vars->win_ptr, vars->img1.ptr, 0, 0);
 
 	return (0);
 }
 
 int press_button(int button_code, int x, int y, t_vars *vars)
 {
-	t_square	sq;
-	int size;
-
-	printf("mlx_ptr = %p\n", vars->mlx_ptr);
-	printf("win_ptr = %p\n", vars->win_ptr);
-	printf("img1 = %p\n", vars->img1.ptr);
-	printf("img2 = %p\n", vars->img2.ptr);
-	//	Just for using the variables:
-	size = button_code + x + y + vars->color;
-	vars->img = 2;
+//	printf("button_code = %i\n", button_code);
+	vars->shape.size += 1 * (button_code == 4) - 1 * (button_code == 5);
+	if (button_code == 1 || button_code == 2)
+	{
+		if (vars->img == 1 || vars->img == 2)
+			fix_shape(vars);
+		if (button_code == 1)
+			vars->shape.type = 'c';
+		else if (button_code == 2)
+			vars->shape.type = 's';
+		vars->img = 1;
+		vars->shape.x = x;
+		vars->shape.y = y;
+	}
 	copy_img(vars);
-	size = 24;
-	sq.x0 = x - size / 2;
-	sq.x1 = sq.x0 + size;
-	sq.y0 = y - size / 2;
-	sq.y1 = sq.y0 + size;
-	square_paint(sq, vars);
+	if (vars->shape.type == 's')
+		square_paint(vars);
+	else if (vars->shape.type == 'c')
+		circle_paint(vars);
 	return (0);
 }
 
 int move_mouse(int x, int y, t_vars *vars)
 {
-//	printf("working on image %i\n", vars->img);
-//	printf("image 1 prt: %p", vars->img1.ptr);
-//	printf("image 2 prt: %p", vars->img2.ptr);
-	
-	t_square	sq;
-	int			size;
-
-	if (vars->img == 2)
+	if (vars->img == 1)
 	{
 		copy_img(vars);
-		size = 24;
-		sq.x0 = x - size / 2;
-		sq.x1 = sq.x0 + size;
-		sq.y0 = y - size / 2;
-		sq.y1 = sq.y0 + size;
-		square_paint(sq, vars);
+		vars->shape.x = x;
+		vars->shape.y = y;
+		if (vars->shape.type == 's')
+			square_paint(vars);
+		else if (vars->shape.type == 'c')
+			circle_paint(vars);
 	}
 	return (0);
 }
 
 int release_button(int button_code, int x, int y, t_vars *vars)
 {
-	t_square	sq;
-	int			size;
-
-	vars->img = 1;
-	size = 24 * button_code;
-	sq.x0 = x - size / 2;
-	sq.x1 = sq.x0 + size;
-	sq.y0 = y - size / 2;
-	sq.y1 = sq.y0 + size;
-	square_paint(sq, vars);
+	button_code = 0;
+	// vars->img = 0;
+	vars->img = 2;
+	vars->shape.x = x;
+	vars->shape.y = y;
 	copy_img(vars);
+	if (vars->shape.type == 's')
+		square_paint(vars);
+	else if (vars->shape.type == 'c')
+		circle_paint(vars);
+//	copy_img(vars);
 
 	return (0);
+}
+
+void	fix_shape(t_vars *vars)
+{
+	vars->img = 0;
+	if (vars->shape.type == 's')
+		square_paint(vars);
+	else if (vars->shape.type == 'c')
+		circle_paint(vars);
+	copy_img(vars);
+}
+
+void	erase_shape(t_vars *vars)
+{
+	vars->img = 0;
+	copy_img(vars);
 }
 
 //int	prueba(int info1, int info2, t_vars *vars)
@@ -286,32 +384,46 @@ int release_button(int button_code, int x, int y, t_vars *vars)
 //	return (0);
 //}
 
-int		main(void)
+void	shape_init(t_vars *vars)
 {
-	t_vars	vars;
-	t_img	img1;
-	t_img	img2;
+	t_shape		shape;
+	
+	shape.type = 'c';
+	shape.size = 24;
+	shape.x = 0;
+	shape.y = 0;
+	shape.mov_x = 0;
+	shape.mov_y = 0;
+	vars->shape = shape;
+}
 
+int	main(void)
+{
+	t_vars		vars;
+	t_img		img0;
+	t_img		img1;
+
+	shape_init(&vars);
 	vars.x_dim = 800;
 	vars.y_dim = 600;
 	vars.color = 0;
+	vars.img = 0;
 	vars.mlx_ptr = mlx_init();
 	vars.win_ptr = mlx_new_window(vars.mlx_ptr, vars.x_dim, vars.y_dim, "Movement");
 	mlx_hook(vars.win_ptr, 2, 1L<<0, press_key, &vars);
 	mlx_hook(vars.win_ptr, 4, 0L, press_button, &vars);
 	mlx_hook(vars.win_ptr, 6, 0L, move_mouse, &vars);
 	mlx_hook(vars.win_ptr, 5, 0L, release_button, &vars);
-//	mlx_hook(vars.win_ptr, 3, 1L<<1, release_key, &vars);
+	mlx_hook(vars.win_ptr, 3, 1L<<1, release_key, &vars);
 //	mlx_hook(vars.win_ptr, 17, 0L, prueba, &vars);
 
+	img0.ptr = mlx_new_image(vars.mlx_ptr, vars.x_dim, vars.y_dim);
+	img0.addr = mlx_get_data_addr(img0.ptr, &img0.bpp, &img0.lnlen, &img0.endian);
 	img1.ptr = mlx_new_image(vars.mlx_ptr, vars.x_dim, vars.y_dim);
 	img1.addr = mlx_get_data_addr(img1.ptr, &img1.bpp, &img1.lnlen, &img1.endian);
-	img2.ptr = mlx_new_image(vars.mlx_ptr, vars.x_dim, vars.y_dim);
-	img2.addr = mlx_get_data_addr(img2.ptr, &img2.bpp, &img2.lnlen, &img2.endian);
+	vars.img0 = img0;
 	vars.img1 = img1;
-	vars.img2 = img2;
 	
-
 	//printf("lnlen = %i\n", img.lnlen);
 	//pixel_push(&img, 0, 0, 0x00FF0000);
 	//mlx_put_image_to_window(vars.mlx_ptr, vars.win_ptr, img.ptr, 0, 0);
@@ -321,6 +433,227 @@ int		main(void)
 
 	return (0);
 }
+
+
+////	Sending motion images to the window
+////	choose a color (1 to 8 keys), and click over the window.
+////	Drag the square where you want it to stay and release the button.
+//
+//typedef struct s_img
+//{
+//	void		*ptr;
+//	char		*addr;
+//	int			bpp;
+//	int			lnlen;
+//	int			endian;
+//}			t_img;
+//
+//typedef struct s_vars
+//{
+//	void		*mlx_ptr;
+//	void		*win_ptr;
+//	t_img		img1;
+//	t_img		img2;
+//	int			img;
+//	int			x_dim;
+//	int			y_dim;
+//	unsigned	color;
+//}			t_vars;
+//
+//typedef struct s_square
+//{
+//	int			x0;
+//	int			x1;
+//	int			y0;
+//	int			y1;
+//}			t_square;
+//
+//int	press_key(int key_code, t_vars *vars);
+//int	window_area(int key_code, t_vars *vars);
+//int	square_paint(t_square sq, t_vars *vars);
+//void	pixel_push(t_vars *vars, int x, int y);
+//
+//
+//int	press_key(int key_code, t_vars *vars)
+//{
+//	if (key_code == 53)
+//		mlx_destroy_window(vars->mlx_ptr, vars->win_ptr);
+//	else if (key_code == 28)
+//		vars->color = 0x00ffffff;
+//	else if (key_code == 18)
+//		vars->color = 0x00ff0000;
+//	else if (key_code == 19)
+//		vars->color = 0x0000ff00;
+//	else if (key_code == 20)
+//		vars->color = 0x000000ff;
+//	else if (key_code == 21)
+//		vars->color = 0x0000ffff;
+//	else if (key_code == 23)
+//		vars->color = 0x00ff00ff;
+//	else if (key_code == 22)
+//		vars->color = 0x00ffff00;
+//	else if (key_code == 26)
+//		vars->color = 0x00000000;
+//	else if ((key_code <= 126 && key_code >= 123) || key_code == 49)
+//		window_area(key_code, vars);
+//	return (0);
+//}
+//int	window_area(int key_code, t_vars *vars)
+//{
+//	t_square area;
+//
+//	area.x0 = 0 +  (vars->x_dim / 2) * (key_code == 124);
+//	area.x1 = vars->x_dim / (1 + (key_code == 123));
+//	area.y0 = 0 +  (vars->y_dim / 2) * (key_code == 125);
+//	area.y1 = vars->y_dim / (1 + (key_code == 126));
+//	square_paint(area, vars);
+//	return (0);
+//}
+//
+//int square_paint(t_square sq, t_vars *vars)
+//{
+//	int i;
+//	int j;
+//
+//	j = sq.y0;
+//	while (j < sq.y1)
+//	{
+//		i = sq.x0;
+//		while (i < sq.x1)
+//		{
+//			pixel_push(vars, i, j);
+//			i++;
+//		}
+//		j++;
+//	}
+//	return (0);
+//}
+//
+//void	pixel_push(t_vars *vars, int x, int y)
+//{
+//	char	*dst;
+//
+//	if (vars->img == 1)
+//	{
+//		dst = vars->img1.addr + (y * vars->img1.lnlen + x * (vars->img1.bpp / 8));
+//		*(unsigned int*)dst = vars->color;
+//	}
+//	else if (vars->img == 2)
+//	{
+//		dst = vars->img2.addr + (y * vars->img2.lnlen + x * (vars->img2.bpp / 8));
+//		*(unsigned int*)dst = vars->color;
+//	}
+//}
+//
+//void	copy_img(t_vars *vars)
+//{
+//	char	*dst;
+//	int		x;
+//	int		y;
+//
+//	y = 0;
+//	while (y < vars->y_dim)
+//	{
+//		x = 0;
+//		while (x < vars->x_dim)
+//		{
+//			dst = vars->img2.addr + (y * vars->img2.lnlen + x * (vars->img2.bpp / 8));
+//			*(unsigned int*)dst = *(unsigned int*)(vars->img1.addr + (y * vars->img1.lnlen + x //* (vars->img1.bpp / 8)));
+//			x++;
+//		}
+//		y++;
+//	}
+//}
+//
+//int	no_event(t_vars *vars)
+//{
+//	if (vars->img == 1)
+//		mlx_put_image_to_window(vars->mlx_ptr, vars->win_ptr, vars->img1.ptr, 0, 0);
+//	else if (vars->img == 2)
+//		mlx_put_image_to_window(vars->mlx_ptr, vars->win_ptr, vars->img2.ptr, 0, 0);
+//
+//	return (0);
+//}
+//
+//int press_button(int button_code, int x, int y, t_vars *vars)
+//{
+//	t_square	sq;
+//	int size;
+//	//	Just for using the variables:
+//	size = button_code + x + y + vars->color;
+//	vars->img = 2;
+//	copy_img(vars);
+//	size = 24;
+//	sq.x0 = x - size / 2;
+//	sq.x1 = sq.x0 + size;
+//	sq.y0 = y - size / 2;
+//	sq.y1 = sq.y0 + size;
+//	square_paint(sq, vars);
+//	return (0);
+//}
+//
+//int move_mouse(int x, int y, t_vars *vars)
+//{
+//	t_square	sq;
+//	int			size;
+//
+//	if (vars->img == 2)
+//	{
+//		copy_img(vars);
+//		size = 24;
+//		sq.x0 = x - size / 2;
+//		sq.x1 = sq.x0 + size;
+//		sq.y0 = y - size / 2;
+//		sq.y1 = sq.y0 + size;
+//		square_paint(sq, vars);
+//	}
+//	return (0);
+//}
+//
+//int release_button(int button_code, int x, int y, t_vars *vars)
+//{
+//	t_square	sq;
+//	int			size;
+//
+//	vars->img = 1;
+//	size = 24 * button_code;
+//	sq.x0 = x - size / 2;
+//	sq.x1 = sq.x0 + size;
+//	sq.y0 = y - size / 2;
+//	sq.y1 = sq.y0 + size;
+//	square_paint(sq, vars);
+//	copy_img(vars);
+//
+//	return (0);
+//}
+//
+//int		main(void)
+//{
+//	t_vars	vars;
+//	t_img	img1;
+//	t_img	img2;
+//
+//	vars.x_dim = 800;
+//	vars.y_dim = 600;
+//	vars.color = 0;
+//	vars.mlx_ptr = mlx_init();
+//	vars.win_ptr = mlx_new_window(vars.mlx_ptr, vars.x_dim, vars.y_dim, "Movement");
+//	mlx_hook(vars.win_ptr, 2, 1L<<0, press_key, &vars);
+//	mlx_hook(vars.win_ptr, 4, 0L, press_button, &vars);
+//	mlx_hook(vars.win_ptr, 6, 0L, move_mouse, &vars);
+//	mlx_hook(vars.win_ptr, 5, 0L, release_button, &vars);
+//	img1.ptr = mlx_new_image(vars.mlx_ptr, vars.x_dim, vars.y_dim);
+//	img1.addr = mlx_get_data_addr(img1.ptr, &img1.bpp, &img1.lnlen, &img1.endian);
+//	img2.ptr = mlx_new_image(vars.mlx_ptr, vars.x_dim, vars.y_dim);
+//	img2.addr = mlx_get_data_addr(img2.ptr, &img2.bpp, &img2.lnlen, &img2.endian);
+//	vars.img1 = img1;
+//	vars.img2 = img2;
+//	mlx_loop_hook(vars.mlx_ptr, no_event, &vars);
+//	mlx_loop(vars.mlx_ptr);
+//
+//	return (0);
+//}
+
 
 //// Sending static images to the window
 //// Choose the color to send (rgb cmyk w) with the letter key, and press a direction arrow or space key to send it.
